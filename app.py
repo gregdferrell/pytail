c043254dabe24667957d3c19be38295017a66b1c
greg ferrell <gferrell20@gmail.com>
2018-11-06 22:47:24 -0400

    complete first impl with some tests

---
+++ app.py
@@ -0,0 +1,47 @@
+import argparse
+import os
+import shutil
+import sys
+
+
+def get_input_args():
+    """
+    Retrieves and parses the command line arguments created and defined using the argparse module.
+    :return: parse_args() -data structure that stores the command line arguments object
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_path", help="the path to the file to tail")
+    parser.add_argument("-n", "--num-lines", help="the number of lines to print", default=5)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def tail_from(f, lines_remaining):
+    f.seek(0, os.SEEK_END)
+    while lines_remaining > 0:
+        try:
+            f.seek(f.tell() - 1, os.SEEK_SET)
+        except ValueError:
+            # If we get to beginning of file, break
+            break
+        ch = f.read(1)
+        if ch == os.linesep:
+            lines_remaining -= 1
+        if lines_remaining > 0:
+            f.seek(f.tell() - 1, os.SEEK_SET)
+
+
+def main():
+    args = get_input_args()
+
+    full_file_path = os.path.join(os.path.join(os.path.abspath(os.path.dirname(__file__)), args.file_path))
+
+    with open(full_file_path, 'r') as f:
+        tail_from(f, int(args.num_lines))
+        shutil.copyfileobj(f, sys.stdout)
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_app.py
@@ -0,0 +1,29 @@
+import os
+
+from app import tail_from
+
+
+def test_tail_from_1line_n1():
+    full_file_path = os.path.join(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'files/test-utf8-1line.txt'))
+
+    tail_output = None
+    with open(full_file_path, 'r') as f:
+        tail_from(f, lines_remaining=1)
+        tail_output = f.readlines()
+
+    assert tail_output[0] == '1abc'
+
+
+def test_tail_from_5line_n5():
+    full_file_path = os.path.join(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'files/test-utf8-5line.txt'))
+
+    tail_output = None
+    with open(full_file_path, 'r') as f:
+        tail_from(f, lines_remaining=5)
+        tail_output = f.readlines()
+
+    assert tail_output[0] == '1abc\n'
+    assert tail_output[1] == '2abc\n'
+    assert tail_output[2] == '3abc\n'
+    assert tail_output[3] == '4abc\n'
+    assert tail_output[4] == '5abc'
